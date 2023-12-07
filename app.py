2afc9852be92c874dafb1108d3508fefa2e66d41
sheen-park <118971066+sheen-park@users.noreply.github.com>
2023-12-07 14:01:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
-st.header("Hellow!! 안녕!!@@")
-print("another program")
+st.title(" this is a Title")
+st.header(" This is a Header")
+st.subheader("thsi is subheader")
+st.write("This is text with write  method")
+st.caption("This is a caption")
+
+with open("./contents/REAM.md","r", encoding='UTF-8") as f:
+          markdown_text=f.read()
+st.markdown(markdown_text, unsafe_allow_html=True)
